71af0aad630b8eca07123e99a475c98e0fc55bfd
Unknown <bastien.chies@epsi.fr>
2018-04-15 19:50:12 +0200

    [Fixed] Redirection login -> home
    La redirection ne s'effectuait pas, la vue du controller home s'affichait sur l'url: /login.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -14,6 +14,7 @@ class HomeController extends Controller
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'session'   => $_SESSION['_sf2_attributes']
         ]);
     }
 }
