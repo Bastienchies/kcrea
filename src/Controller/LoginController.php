ba65e578f46752e7ceca2c8612313122a08bb512
Unknown <bastien.chies@epsi.fr>
2018-04-15 18:37:33 +0200

    Ajout du backend connexion.
    Redirection vers la page home avec la variable de session, ajout d'une image par défaut.

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -10,10 +16,66 @@ class LoginController extends Controller
     /**
      * @Route("/login", name="login")
      */
-    public function index()
+    public function index(Request $request)
     {
+        if(isset($_SESSION['_sf2_attributes']['nom'])) {
+            return $this->render('home/index.html.twig', array(
+                'session'   => $_SESSION['_sf2_attributes']
+            ));
+        }
+
+        $user = new Utilisateur();
+
+        $form = $this->createFormBuilder()
+            ->add('email_utilisateur', EmailType::class, [
+                'attr' => ['class' => 'form-control fc-outline-dark','name' => 'mail','placeholder' => 'Adresse mail'],
+                'required' => 'true',
+            ])
+            ->add('password_utilisateur', PasswordType::class, [
+                'attr' => ['class' => 'form-control fc-outline-dark','name'=>'password','placeholder' => 'Mot de passe'],
+                'required' => 'true',
+            ])
+            ->add('connexion', SubmitType::class, [
+                'attr' => ['class' => 'btn btn-info btn-block','name' => 'password','type'=>'submit', 'value' => 'Connexion'],
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $credentials = $form->getData();
+            $hashedpass = hash('sha256',$credentials['password_utilisateur']);
+            $repository = $this->getDoctrine()->getRepository(Utilisateur::class);
+
+            if($repository != null) {
+                //echo 'cas 1';
+                $utilisateur = $repository->findOneBy([
+                    'email_utilisateur' => $credentials['email_utilisateur'],
+                    'password_utilisateur' => $hashedpass,
+                ]);
+                if($utilisateur != null) {
+                    $session = new Session();
+                    $session->set('nom', $utilisateur->getNomUtilisateur());
+                    $session->set('prenom', $utilisateur->getPrenomUtilisateur());
+                    $session->set('email', $utilisateur->getEmailUtilisateur());
+                    $session->set('avatar', $utilisateur->getAvatarUtilisateur());
+                    return $this->render('home/index.html.twig', array(
+                        'session'   => $_SESSION['_sf2_attributes']
+                    ));
+                }
+            }
+            else
+            {
+                return $this->render('login/login.html.twig',array(
+                    'form' => $form->createView(),
+                    'errorMessage' => 'Identifiants invalides.'
+                ));
+            }
+        }
+
         return $this->render('login/index.html.twig', [
             'controller_name' => 'LoginController',
+            'form' => $form->createView(),
         ]);
     }
 }
